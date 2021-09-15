57d4ebc1c8fe91334e1dae99c953748ebda8fcaa
Ali Sbane <ali.sbane.dev@gmail.com>
2021-09-15 11:26:19 +0100

    Third Section Drop Down functionality finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,12 @@
 function init() {
   window.addEventListener("load", startScript);
+  let windowWidth = window.innerWidth;
 
   function startScript() {
-    openCloseMenu();
+    if(windowWidth < 768) {
+      openCloseMenu();
+      thirdSectionDropDown()
+    }
   }
 
   // Menu func
@@ -22,6 +26,35 @@ function init() {
         humIcon.classList.add("close-state");
       }
     });
+  }
+
+  // third section drop down func
+  function thirdSectionDropDown() {
+    let dropDownPart = document.querySelector('.third__menu-wrapper');
+    let dropDownBox = document.querySelector('.third__drop-down-box');
+    let selected = document.querySelector('.third__menu__tab--active');
+
+    let dropDownPartHeight = dropDownPart.scrollHeight;
+
+    dropDownPart.classList.add("third__menu-wrapper-hide");
+
+    dropDownBox.addEventListener("click", () => {
+      if(dropDownPart.classList.contains("third__menu-wrapper-hide")) {
+        dropDownPart.style.height = dropDownPartHeight + "px";
+        dropDownPart.classList.remove("third__menu-wrapper-hide");
+      } else {
+        dropDownPart.style.height = "0px";
+        dropDownPart.classList.add("third__menu-wrapper-hide");
+      }
+    });
+
+    dropDownPart.addEventListener("click", (e) => {
+      let previousChoiceText = selected.textContent;
+      let choice = e.target;
+      selected.textContent = choice.textContent;
+      choice.textContent = previousChoiceText;
+    });
+
   }
 
 }
